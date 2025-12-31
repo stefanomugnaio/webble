09412350eceaf37488e8b9f5163e5e746936592a
stefanomugnaio <stephane.mn@outlook.fr>
2025-12-31 16:18:02 +0100

    Revue de l'accès au form d'enregsitrement

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -5,21 +5,33 @@ namespace App\Controller;
 use App\Entity\Client;
 use App\Form\RegistrationFormType;
 use App\Service\RegistrationService;
+use App\Repository\ClientRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Bundle\SecurityBundle\Security;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
-use Symfony\Component\Security\Http\Attribute\IsGranted;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class RegistrationController extends AbstractController
 {
     #[Route('/enregistrement', name: 'app_register')]
-    #[IsGranted('ROLE_ADMIN')]
     public function register(
         Request $request,
-        RegistrationService $clientService
+        RegistrationService $clientService,
+        ClientRepository $clientRepository,
+        Security $security
     ): Response {
-        
+
+        // --------- CONTRÔLE D'ACCÈS SPÉCIAL ----------
+        // S'il y a déjà des clients en base,
+        // seul un ROLE_ADMIN peut accéder à cette page.
+        $nbClients = $clientRepository->count([]);
+
+        if ($nbClients > 0 && !$security->isGranted('ROLE_ADMIN')) {
+            throw $this->createAccessDeniedException();
+        }
+        // ---------------------------------------------
+
         $client = new Client();
 
         // Création du formulaire
